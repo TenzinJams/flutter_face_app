8e3e6c1eca2c234777a073d0f7a7feda9021bbf0
TenzinJams <tenzinjamtsho108@gmail.com>
2024-05-08 00:55:26 +0600

    remove titlebar

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Hello World App'),
-        ),
-        body: const Center(
+        body: Center(
           child: Text(
             'Hello, World!',
             style: TextStyle(fontSize: 24),
