eda4ff5cf217766140fc255f369599bd2142767d
TenzinJams <tenzinjamtsho108@gmail.com>
2024-05-08 16:16:19 +0600

    implemented image_picker plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'dart:io';
+import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter_mediapipe/flutter_mediapipe.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +26,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +40,6 @@ class MyHomePage extends StatelessWidget {
           children: <Widget>[
             ElevatedButton(
               onPressed: () {
-                // Navigate to register screen
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => RegisterScreen()),
@@ -51,7 +56,7 @@ class MyHomePage extends StatelessWidget {
                   MaterialPageRoute(builder: (context) => LoginScreen()),
                 );
               },
-              child: Text('Login'),
+              child: const Text('Login'),
             ),
           ],
         ),
@@ -60,8 +65,79 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
-class RegisterScreen extends StatelessWidget {
-  const RegisterScreen({super.key});
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({Key? key}) : super(key: key);
+
+  @override
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  XFile? _imageFile;
+  final MediaPipeFaceDetector _faceDetector = MediaPipeFaceDetector();
+
+  @override
+  void initState() {
+    super.initState();
+    _faceDetector.loadModel();
+  }
+
+  @override
+  void dispose() {
+    _faceDetector.close();
+    super.dispose();
+  }
+
+  Future<void> _takePicture() async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? image = await _picker.pickImage(source: ImageSource.camera);
+    setState(() {
+      _imageFile = image;
+    });
+  }
+
+  Future<void> _detectFaces() async {
+    if (_imageFile == null) return;
+
+    final File file = File(_imageFile!.path);
+    final List<FaceDetection> faces =
+        await _faceDetector.detectFromBinary(file.readAsBytesSync());
+
+    setState(() {
+      _imageFile = _imageFile; // Refresh to trigger repaint
+    });
+
+    final ui.Image image = await loadImage(file);
+
+    final painter = FacePainter(
+      faces: faces,
+    );
+
+    final ui.PictureRecorder recorder = ui.PictureRecorder();
+    final Canvas canvas = Canvas(recorder);
+    canvas.drawImage(image, Offset.zero, Paint());
+    painter.paint(
+        canvas, Size(image.width.toDouble(), image.height.toDouble()));
+
+    final ui.Picture picture = recorder.endRecording();
+    final ui.Image img = await picture.toImage(image.width, image.height);
+
+    final ByteData? byteData =
+        await img.toByteData(format: ui.ImageByteFormat.png);
+    if (byteData != null) {
+      final buffer = byteData.buffer;
+      // Save the image or use it as desired
+    }
+  }
+
+  Future<ui.Image> loadImage(File file) async {
+    final Uint8List data = await file.readAsBytes();
+    final Completer<ui.Image> completer = Completer();
+    ui.decodeImageFromList(Uint8List.view(data.buffer), (ui.Image img) {
+      return completer.complete(img);
+    });
+    return completer.future;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -69,15 +145,35 @@ class RegisterScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Register'),
       ),
-      body: const Center(
-        child: Text('Register Screen'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: _takePicture,
+              child: const Text('Take Selfie'),
+            ),
+            const SizedBox(height: 20),
+            if (_imageFile != null)
+              Image.file(
+                File(_imageFile!.path),
+                width: 200,
+                height: 200,
+              ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _detectFaces,
+              child: const Text('Detect Faces'),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -89,5 +185,32 @@ class LoginScreen extends StatelessWidget {
         child: Text('Login Screen'),
       ),
     );
+  }
+}
+
+class FacePainter extends CustomPainter {
+  final List<FaceDetection> faces;
+
+  FacePainter({required this.faces});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Paint paint = Paint()
+      ..color = Colors.green
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 3.0;
+
+    for (final face in faces) {
+      final Rect rect = Rect.fromPoints(
+        Offset(face.left, face.top),
+        Offset(face.right, face.bottom),
+      );
+      canvas.drawRect(rect, paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
